599dffb4764027f83e11bc26ff209ede6b3abee6
jack20492 <nphuc210@gmail.com>
2024-10-05 19:32:06 +0700

    up lan 4'

--- js_1/Employee.js
+++ js_1/Employee.js
@@ -1,12 +1,48 @@
 class Employee {
-  constructor(taiKhoan, hoTen, email, ngayLam, chucVu, luongCoBan, gioLam) {
+  constructor(
+    taiKhoan,
+    hoTen,
+    email,
+    matKhau,
+    ngayLam,
+    luongCoBan,
+    chucVu,
+    gioLam
+  ) {
     this.taiKhoan = taiKhoan;
     this.hoTen = hoTen;
     this.email = email;
+    this.matKhau = matKhau;
     this.ngayLam = ngayLam;
     this.chucVu = chucVu;
     this.luongCoBan = luongCoBan;
     this.gioLam = gioLam;
+    this.tongLuong = 0;
+    this.xepLoai = "";
+  }
+
+  // Tính tổng luong
+  calTotalSalary() {
+    if (this.chucVu === "Giám đốc") {
+      this.tongLuong = this.luongCoBan * 3;
+    } else if (this.chucVu === "Trưởng Phòng") {
+      this.tongLuong = this.luongCoBan * 2;
+    } else {
+      this.tongLuong = this.luongCoBan;
+    }
+  }
+
+  // Xếp loại nhân viên
+  rankEmployee() {
+    if (this.gioLam >= 192) {
+      this.xepLoai = "Xuất sắc";
+    } else if (this.gioLam >= 176) {
+      this.xepLoai = "Giỏi";
+    } else if (this.gioLam >= 160) {
+      this.xepLoai = "Khá";
+    } else {
+      this.xepLoai = "Trung bình";
+    }
   }
 }
 
--- js_1/Index.js
+++ js_1/Index.js
@@ -1,13 +1,55 @@
-import Employee from "./js_1/Employee.js";
-import ManageEmployee from "./js_1/ManageEmployee.js";
+import Employee from "./../js_1/Employee.js";
+import ManageEmployee from "./../js_1/ManageEmployee.js";
 
-const ManageEmployee = new ManageEmployee();
+const manageEmployee = new ManageEmployee();
 
 const getEleId = (id) => document.getElementById(id);
 
-// Thêm nhân viên
-const renderEmployee = (listEmployee) => {
+// Lấy thông tin nhâ nviên nhập vào
+getEleId("btnThemNV").onclick = () => {
+  // Lấy thông tin nhân viên tù bảng
+  const taiKhoan = getEleId("tknv").value;
+  const hoTen = getEleId("name").value;
+  const email = getEleId("email").value;
+  const matKhau = getEleId("password").value;
+  const ngayLam = getEleId("datepicker").value;
+  const luongCoBan = getEleId("luongCB").value;
+  const chucVu = getEleId("chucvu").value;
+  const gioLam = getEleId("gioLam").value;
+
+  // Create new Employee object
+  const employee = new Employee(
+    taiKhoan,
+    hoTen,
+    email,
+    matKhau,
+    ngayLam,
+    luongCoBan,
+    chucVu,
+    gioLam
+  );
+
+  manageEmployee.addEmployee(employee);
+
+  renderEmployee(manageEmployee.employeeList);
+};
+
+// In thông tin nhân viên ra danh sách table
+
+const renderEmployee = (employeeList) => {
   let contentHTML = "";
-  listEmployee.forEach((Employee) => {
-    
+  employeeList.foreach((employee) => {
+    contentHTML += `
+        <tr>
+          <td>${employee.taiKhoan}</td>
+          <td>${employee.hoTen}</td>
+          <td>${employee.email}</td>
+          <td>${employee.ngayLam}</td>
+          <td>${employee.chucVu}</td>
+          <td>${employee.tongLuong}</td>
+          <td>${employee.xepLoai}</td>
+        </tr>
+    `;
+  });
+  getEleId("tableDanhSach").innerHTML = contentHTML;
 };
